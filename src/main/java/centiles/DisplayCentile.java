14822adb5599c09679614ba422778b020d3feefb
Douglas Luman <dluman@douglasjluman.com>
2019-10-11 10:24:42 -0400

    Initial commit.

---
+++ src/main/java/centiles/DisplayCentile.java
@@ -0,0 +1,33 @@
+package fractions;
+
+import java.util.Scanner;
+
+/** Creates fractions, displays as centiles.
+ *
+ * @author {YOUR NAME HERE}
+ */
+public class DisplayCentile {
+  
+  /** Entry point.
+   *
+   * @param args The command line arguments
+   */
+  public static void main(String[] args) {
+    /*
+     * TODO Implement scanner to read from System.in
+     */
+    System.out.print("Enter points earned: ");
+    int earned = // TODO Read an integer
+    System.out.print("Enter points possible: ");
+    int possible = //TODO Read an integer
+    /*
+     * TODO Create fraction representation of score
+     */
+    /*
+     * TODO Get "centile" representation of score
+     */
+    /*
+     * TODO Print output according to README
+     */
+  }
+}
---
+++ src/main/java/centiles/Fraction.java
@@ -0,0 +1,55 @@
+package fractions;
+
+import java.text.NumberFormat;
+
+/** Manages creation of Fractions and their centiles.
+ *
+ * @author {Your Name Here}
+ */
+public class Fraction {
+  
+  public final int numer;
+  public final int denom;
+  
+  /** Constructor.
+   *
+   * @param numer Numerator
+   * @param denom Denominator
+   */
+  public Fraction(int numer, int denom) {
+    this.numer = numer;
+    this.denom = denom;
+  }
+  
+  /** Creates a fraction with 100 as denominator.
+   *
+   */
+  public Fraction centile() {
+    int numer = (this.numer * 100) / this.denom;
+    /*
+     * TODO Use the above value to create a Fraction
+     *      representing the value as a centile, called
+     *      "centler" (identifier)
+     */
+    return centile;
+  }
+  
+  /** Formats the fraction as a percentage.
+   *
+   */
+  public String fmtPercent() {
+    NumberFormat fmt = NumberFormat.getPercentInstance();
+    fmt.setMinimumFractionDigits(1);
+    double quotient = this.numer / this.denom; // TODO Fix this line; something's not right...
+    return fmt.format(quotient);
+  }
+  
+  /** Creates the String form of a fraction.
+   *
+   */
+  public String toString() {
+    String fraction = // TODO Implement the string form of a fraction.
+    return fraction;
+  }
+    
+}
